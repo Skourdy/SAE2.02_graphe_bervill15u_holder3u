a23b5c659f5b7a86dda16e1e0607c0bcce268c36
Jules <jules.holder.54@gmail.com>
2022-06-07 08:57:40 +0200

    ajout import liste dans noeud

---
+++ graphe/src/GrapheListe.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+/**
+ * Classe GrapheListe
+ */
+public class GrapheListe implements Graphe {
+    //Attributs
+    //Liste des noms des noeuds
+    List<String> ensNom;
+    //Liste des noeuds
+    List<Noeud> ensNoeud;
+
+    //Méthode listeNoeuds()
+    //Retourne la liste des noms de noeuds
+    @Override
+    public List<String> listeNoeuds() {
+        return ensNom;
+    }
+
+
+    /**
+     * Méthode suivants(String n)
+     * Retourne la liste des arcs partant du noeud n
+     */
+    @Override
+    public List<Arc> suivants(String n) {
+        if (ensNom.contains(n)) {
+            return ensNoeud.get(ensNom.indexOf(n)).getAdj();
+        }
+
+    }
+
+    public void ajouterArc(String n1, String n2, double poids) {
+
+    }
+}
--- graphe/src/Noeud.java
+++ graphe/src/Noeud.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Noeud{
 
     private String nom;
