c4983ced530142762a983b430946238ecb9926df
Thomas Berville <thomas.berville6@etu.univ-lorraine.fr>
2022-06-07 11:21:40 +0200

    test toGraphviz

--- graphe/test/GrapheListeTest.java
+++ graphe/test/GrapheListeTest.java
@@ -76,7 +76,47 @@ class GrapheListeTest {
 
     @Test
     void test_toGraphviz(){
+        List<String> nom = new ArrayList<>();
+        nom.add("A");
+        nom.add("B");
+        nom.add("C");
+        nom.add("D");
+        nom.add("E");
 
+        List<Noeud> noeuds = new ArrayList<>();
+        Noeud nA = new Noeud("A");
+        nA.ajouterArc("B",12);
+        nA.ajouterArc("D",87);
+        noeuds.add(nA);
+
+        Noeud nB = new Noeud("B");
+        nB.ajouterArc("E",11);
+        noeuds.add(nB);
+
+        Noeud nC = new Noeud("C");
+        nC.ajouterArc("A",19);
+        noeuds.add(nC);
+
+        Noeud nD = new Noeud("D");
+        nD.ajouterArc("B",23);
+        nD.ajouterArc("C",10);
+        noeuds.add(nD);
+
+        Noeud nE = new Noeud("E");
+        nE.ajouterArc("D",43);
+        noeuds.add(nE);
+
+        GrapheListe lg = new GrapheListe(nom,noeuds);
+
+        assertEquals("digraph {\n"+
+            "A -> B [label = 12] \n"+
+            "A -> D [label = 87] \n"+
+            "B -> E [label = 11] \n"+
+            "C -> A [label = 19] \n"+
+            "D -> B [label = 23] \n"+
+            "D -> C [label = 10] \n"+
+            "E -> D [label = 43] \n"+
+        "}",lg.toGraphViz());
     }
 
 }
